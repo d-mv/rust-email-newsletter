2d9ea63a0cd814e4dde42b00319d7720ce197ff5
Dmitry Melnikov <d.dash.mv@gmail.com>
2021-04-14 10:10:33 +0300

    Updated

---
+++ tests/health_check.rs
@@ -0,0 +1,27 @@
+// `actix_rt::test` is the testing equivalent of `actix_web::main`.
+// It also spares you from having to specify the `#[test]` attribute.
+//
+// Use `cargo add actix-rt --dev --vers 2` to add `actix-rt`
+// under `[dev-dependencies]` in Cargo.toml
+//
+// You can inspect what code gets generated using
+// `cargo expand --test health_check` (<- name of the test file)
+#[actix_rt::test]
+async fn health_check_works() {
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = email_newsletter::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
